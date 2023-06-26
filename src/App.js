cedc1c2426cb7a68d836be470529b3f7f262f3f9
maan.s <gurri.singh8686@gmail.com>
2023-06-26 12:21:33 +0530

    Changed App file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactt
+          Hello world
         </a>
       </header>
     </div>
