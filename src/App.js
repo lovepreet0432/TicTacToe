be85f99220e050d178d8519801a703a4626a74ca
maan.s <gurri.singh8686@gmail.com>
2023-06-26 12:42:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello world to new feature
+          test
         </a>
       </header>
     </div>
